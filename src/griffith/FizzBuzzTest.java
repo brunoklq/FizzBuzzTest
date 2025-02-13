ac620ba296445528c91659b628bc785e6bb526e5
Bruno Kennedy <bruno.klqueiroz@gmail.com>
2025-02-13 13:19:49 +0000

    added new features

--- src/griffith/FizzBuzzTest.java
+++ src/griffith/FizzBuzzTest.java
@@ -1,44 +1,26 @@
 package griffith;
+import org.junit.Test;
 
-
-
-import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
 
 public class FizzBuzzTest {
-	
-	
-	@Test
-	
-	public void test() {
 
-	}
 
-	
-	@Test
-	public void execute() {
-		
+	  @Test
+	    public void test() {
+	    }
 
-	}
-	
-	public String processNumber(int number) {
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		
-		assertThat(fizzBuzz.processNumber(1), is("1"));
-		
-		return "1";
-	}
-	
-	@Test
-	public void shouldProcessInput() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		
-		String output = fizzBuzz.processNumber(1);
-		
-		assertThat(output, is("1"));
-		
-		
-	}
-	
+	  
+	  public void execute() {
+	    }
+	  
+	  @Test
+	  public void shouldProcessInput() {
+	      FizzBuzz fizzBuzz = new FizzBuzz();
 
-}	
+	      String output = fizzBuzz.processNumber(1);
 
+	      assertThat(output, is("1"));
+	  }
+}
