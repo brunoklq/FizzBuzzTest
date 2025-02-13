f70755d5be17445433facfa662fe6b2cc239913a
Bruno Kennedy <bruno.klqueiroz@gmail.com>
2025-02-13 12:56:57 +0000

    adding new features for code as inputs

--- src/griffith/FizzBuzzTest.java
+++ src/griffith/FizzBuzzTest.java
@@ -1,7 +1,39 @@
 package griffith;
 
+import org.junit.jupiter.api.Test;
+
 public class FizzBuzzTest {
 	
 	
+	@Test
+	
+	public void test() {
 
-}
+	}
+
+	
+	@Test
+	public void execute() {
+		
+
+	}
+	
+	public String processNumber(int number) {
+		
+		return "1";
+	}
+	
+	@Test
+	public void shouldProcessInput() {
+		FizzBuzz fizzBuzz = new FizzBuzz();
+		
+		String output = fizzBuzz.processNumber(1);
+		
+		assertThat(output, is("1"));
+		
+		
+	}
+	
+
+}	
+
